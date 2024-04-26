4984fbbdc7cd9ff307cd5dd51548abdf49a9e164
WwEIiC <1073780876@qq.com>
2024-04-26 22:47:11 +0800

    [Change]  -don't set the value of staticruntime.

--- premake5.lua
+++ premake5.lua
@@ -2,7 +2,6 @@ project "Box2D"
 	kind "StaticLib"
 	language "C++"
 	cppdialect "C++20"
-    staticruntime "on"
 
 	targetdir ("bin/" .. outputDir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputDir .. "/%{prj.name}")
